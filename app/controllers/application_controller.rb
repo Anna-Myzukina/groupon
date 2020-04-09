d0cef44a6d15e6213100c7c63d08f4e857ec370d
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-04-09 18:17:28 +0300

    Test application with run command rails s after adding hello world to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello world"
+    render html: "Hello world"
   end
 end
