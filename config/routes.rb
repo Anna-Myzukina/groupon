e433c4ab7162b2f441edfeadcbe8e66cd2116f49
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-04-10 19:19:58 +0300

    Add main styles to user pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  root 'static_pages#home'
+
   get 'static_pages/home'
 
   get 'static_pages/profile'
 
-  root 'application#hello'
+
+  resources :users
 end
