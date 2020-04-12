4546d0a14cd9b3416feb5f312779107403e3c6b7
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-04-12 19:00:03 +0300

    Fix errors in session_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(name: params[:session][:name])
+    if @user
+      login_url @user
+      flash[:success] = 'Successfully logged in!'
+      redirect_to @user
+    else
+      flash[:success] = 'user not found!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
