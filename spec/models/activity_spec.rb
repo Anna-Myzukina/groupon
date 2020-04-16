32609a873dcf98f7ea0cd0715bc7c59c2a7ff17d
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-04-16 23:52:52 +0300

    Add rspec tests to project

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @activity = Activity.create(name: 'running')
+  end
+
+  it 'is valid with name of activity and hours of activity' do
+    expect(@activity).to be_valid
+  end
+
+  it 'is invalid without name of activity and hours of activity' do
+    activity = Activity.new(name: nil)
+    activity.valid?
+    expect(activity.errors[:name]).to include("can't be blank")
+  end
 end
